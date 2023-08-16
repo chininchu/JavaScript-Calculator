b06bc5504adfa858e3cd62c22d50b50344391667
chininchu <120990876+chininchu@users.noreply.github.com>
2023-08-16 17:47:22 +0500

    Created a function called clearInputs

--- main.js
+++ main.js
@@ -3,39 +3,57 @@
 
 // Wait until the HTML document is fully loaded before running the code
 
+$(document).ready(function () {
 
-// Initialize a variable to keep track of the current input (leftOperand, operator, or rightOperand)
 
-let currentInput = 'leftOperand';
+    let currentInput = 'leftOperand';
 
 // Function to insert a number into the current input field
 
 
-function insertNumber(number) {
+    function insertNumber(number) {
 
-    // Concatenate the new number with the existing value in the current input field
+        // Concatenate the new number with the existing value in the current input field
 
-    $('#' + currentInput).val($('#' + currentInput).val() + number);
+        $('#' + currentInput).val($('#' + currentInput).val() + number);
 
-    // Update the current input based on the existing state
+        // Update the current input based on the existing state
 
 
-    if (currentInput === 'leftOperand') currentInput = 'operator';
-    else if (currentInput === 'operator') currentInput = 'rightOperand';
-}
+        if (currentInput === 'leftOperand') currentInput = 'operator';
+        else if (currentInput === 'operator') currentInput = 'rightOperand';
+    }
 
 // Function to insert an operator into the operator input field
 
-function insertOperator(operator) {
+    function insertOperator(operator) {
 
-    // Only insert the operator if the current input is the operator field
-    if (currentInput !== 'operator') return;
+        // Only insert the operator if the current input is the operator field
+        if (currentInput !== 'operator') return;
 
-    $('#' + currentInput).val(operator);
-    currentInput = 'rightOperand';
+        $('#' + currentInput).val(operator);
+        currentInput = 'rightOperand';
 
 
-}
+    }
+
+// Function to clear all input fields and reset the current input state
+
+    function clearInputs() {
+
+        $('#leftOperand').value(' ');
+        $('#operator').value(' ');
+        $('#rightOperand').value(' ');
+        currentInput = 'leftOperand';
+
+
+    }
+
+
+});
+
+
+
 
 
 
