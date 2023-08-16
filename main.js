af2daee9cb4c3d841dd533495a669ec1efff458a
chininchu <120990876+chininchu@users.noreply.github.com>
2023-08-16 17:20:07 +0500

    Created a function called insertOperator

--- main.js
+++ main.js
@@ -1,48 +1,41 @@
 "use strict";
 
 
-
 // Wait until the HTML document is fully loaded before running the code
 
-$(document).ready(function () {
 
-    // Initialize a variable to keep track of the current input (leftOperand, operator, or rightOperand)
+// Initialize a variable to keep track of the current input (leftOperand, operator, or rightOperand)
 
-    let currentInput = 'leftOperand';
+let currentInput = 'leftOperand';
 
-    // Function to insert a number into the current input field
-
-
-    function insertNumber(number) {
-
-        // Concatenate the new number with the existing value in the current input field
-
-        $('#' + currentInput).val($('#' + currentInput).val() + number);
-
-        // Update the current input based on the existing state
+// Function to insert a number into the current input field
 
 
-        if (currentInput === 'leftOperand') currentInput = 'operator';
-        else if (currentInput === 'operator') currentInput = 'rightOperand';
-    }
+function insertNumber(number) {
+
+    // Concatenate the new number with the existing value in the current input field
+
+    $('#' + currentInput).val($('#' + currentInput).val() + number);
+
+    // Update the current input based on the existing state
+
+
+    if (currentInput === 'leftOperand') currentInput = 'operator';
+    else if (currentInput === 'operator') currentInput = 'rightOperand';
+}
+
+// Function to insert an operator into the operator input field
+
+function insertOperator(operator) {
+
+    // Only insert the operator if the current input is the operator field
+    if (currentInput !== 'operator') return;
+
+    $('#' + currentInput).val(operator);
+    currentInput = 'rightOperand';
+
+
+}
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-    $('button').click(function () {
-        let value = $(this).text();
-        if (!isNaN(value)) insertNumber(value);
-        // Add additional logic for handling other button clicks
-    });
-});
